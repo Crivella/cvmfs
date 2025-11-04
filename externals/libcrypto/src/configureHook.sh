cf4a944981a3ada7efef39e08a5a3903a5fe5a4b
crivella <davide.grassano@epfl.ch>
2025-11-04 11:20:37 +0100

    Revert "crosscompile libressl"
    This reverts commit 1c8206659d0340f8f9f2e3fea748b1800d61a678.

--- externals/libcrypto/src/configureHook.sh
+++ externals/libcrypto/src/configureHook.sh
@@ -19,9 +19,8 @@ esac
 ############################
 
 mkdir build && cd build
-CFLAGS="${CVMFS_BASE_C_FLAGS} -fPIC -Wno-error=implicit-function-declaration" ../configure \
+CFLAGS="${CVMFS_BASE_C_FLAGS} -fPIC" ../configure \
   --enable-static \
   --disable-shared \
   --disable-tests $DISABLE_ASM \
-  --prefix=${EXTERNALS_INSTALL_LOCATION}/crypto \
-  --host=aarch64-unknown-linux-android
+  --prefix=${EXTERNALS_INSTALL_LOCATION}/crypto
