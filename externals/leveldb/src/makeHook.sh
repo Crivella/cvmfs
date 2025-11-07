a7f88b37a4ffd33d5ed53fba26b69188f8c75e13
crivella <davide.grassano@epfl.ch>
2025-11-07 10:31:17 +0100

    crosscompile leveldb

--- externals/leveldb/src/makeHook.sh
+++ externals/leveldb/src/makeHook.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+find . -name '*.cc' -exec sed -i 's/_unlocked//g' {} \;
+
 make clean
-CFLAGS="$CVMFS_BASE_C_FLAGS -fPIC" CXXFLAGS="$CVMFS_BASE_CXX_FLAGS -fPIC" make -j ${CVMFS_BUILD_EXTERNAL_NJOBS}
+CFLAGS="$CVMFS_BASE_C_FLAGS -fPIC" CXXFLAGS="$CVMFS_BASE_CXX_FLAGS -fPIC --target=${CLANG_TARGET_TRIPLE}" make -j ${CVMFS_BUILD_EXTERNAL_NJOBS}
 strip -S libleveldb.a
 
 cp -rv include/leveldb $EXTERNALS_INSTALL_LOCATION/include/
